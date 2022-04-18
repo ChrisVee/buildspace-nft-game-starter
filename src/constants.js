57bae7ebff94afb97817790e4400514d9936b99c
Chris V <CSViglietta@gmail.com>
2022-04-18 03:00:58 +0200

    update to the lastest smart code contract

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = '0x6e4cd55Ef1C16cd80cCe466bB59D5c3d65a0Dc55';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+    return {
+      name: characterData.name,
+      imageURI: characterData.imageURI,
+      hp: characterData.hp.toNumber(),
+      maxHp: characterData.maxHp.toNumber(),
+      attackDamage: characterData.attackDamage.toNumber(),
+    };
+  };
+  
+  export { CONTRACT_ADDRESS, transformCharacterData };
